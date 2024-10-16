08a6735b9bdcd38c6f87b1ef32c5aaa91fe2847d
Your Name <you@example.com>
2024-10-16 15:25:19 +0200

    working on init textures

--- src/helper2.c
+++ src/helper2.c
@@ -3,19 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   helper2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 15:12:03 by leo               #+#    #+#             */
-/*   Updated: 2024/10/13 15:29:29 by leo              ###   ########.fr       */
+/*   Updated: 2024/10/16 15:24:33 by lglauch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-// void 			init_textures(void)
-// {
-// 	tex()->textures[NORTH] = mlx_load_png(get_game()->map.north);
-// 	tex()->textures[SOUTH] = mlx_load_png(get_game()->map.south);
-// 	tex()->textures[WEST] = mlx_load_png(get_game()->map.west);
-// 	tex()->textures[EAST] = mlx_load_png(get_game()->map.east);
-// }
+void	init_textures(void)
+{
+	xpm_t	**textures;
+
+	textures = NULL;
+	textures[NORTH] = mlx_load_xpm42(get_game()->map.north);
+	textures[SOUTH] = mlx_load_xpm42(get_game()->map.south);
+	textures[EAST] = mlx_load_xpm42(get_game()->map.east);
+	textures[WEST] = mlx_load_xpm42(get_game()->map.west);
+}
